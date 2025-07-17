065e597348d56d3d54a3c0dff3f363abf1dca438
nashilaaa <nashillanh@gmail.com>
2025-07-17 12:27:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,34 @@
 import streamlit as st
 import random
 
-st.set_page_config(page_title="Birthday Quest 💖", page_icon="🎂")
+st.set_page_config(page_title="Birthday Quest💖", page_icon="🎂")
 
 # Judul aplikasi
 st.title("🎉 Birthday Quest: Love Level Challenge")
-st.write("Selamat ulang tahun sayang! 🥰 Yuk selesaikan tantangan cinta ini dulu yaa~")
+st.write("Selamat ulang tahun sayang! 🥰 Yuk isi pertanyaan duluu~")
 
 # LEVEL 1
-st.header("💌 Level 1: Tebak Tanggal Jadian")
+st.header("💌 Level 1: Our Anniv Date")
 jawaban1 = st.text_input("Kapan kita jadian? (format: dd-mm)")
 
 if jawaban1:
-    if jawaban1 == "14-02":  # Ganti sesuai tanggal jadian kamu
-        st.success("Waaah kamu inget banget! 🥹💕 Yuk lanjut ke level 2!")
+    if jawaban1 == "09-11":  # Ganti sesuai tanggal jadian kamu
+        st.success("Yeeeaayy kamu inget!🥹💕 Oke lanjuutt!")
 
-        # LEVEL 2
-        st.header("🧠 Level 2: Kuis Cinta")
-        q2 = st.radio("Kalau aku marah, biasanya kamu ngapain?", 
-                      ["Biarin dulu", "Peluk aku", "Beliin makanan", "Ngilang 😭"])
+       # LEVEL 2
+st.header("🧠 Level 2: About You💘")
+q2 = st.radio(
+    "Kegiatan yang paling bikin kamu happy seharian itu apa?",
+    [...]
+)
 
-        if q2:
-            if q2 == "Beliin makanan":
-                st.success("Haha makanan emang penyelamat segalanya 🍕💕")
+if q2:
+    st.balloons
+    st.info("Okey Noted!")
+
                 
                 # LEVEL 3
-                st.header("🎁 Level 3: Pilih Kotak Hadiah")
+                st.header("🎁 Level 3: Choose One..")
                 pilihan_kotak = st.selectbox("Pilih salah satu kotak:", 
                                              ["Kotak A", "Kotak B", "Kotak C"])
                 if st.button("Buka Kotaknya"):
@@ -40,7 +43,7 @@ if jawaban1:
                 # FINAL: Pilih tempat perayaan
                 st.header("🎈 Pilih Tempat Rayain Ulang Tahun")
                 tempat = st.radio("Kamu mau rayain ulang tahunnya di mana nih?", 
-                                  ["🏠 Di rumah aja, sambil ngbrol tapi ada yang recokin",
+                                  ["🏠 Di rumah aja, sambil ngbrol tapi ada yang recokin, heheh",
                                    "🍽️ Romantic Dinner",
                                    "🌌 Jalan-jalan, malmingan",
                                    "🏕️ Rayain di cafe"])
@@ -49,7 +52,7 @@ if jawaban1:
                     st.success(f"Okeee! Nanti kita {tempat.split(' ')[1]} ya sayang 😍")
                     st.snow()
                     st.markdown("## 🥂 Cheers for You!")
-                    st.write("Terima kasih udah main sampai akhir. Aku cinta kamu selalu 💕")
+                    st.write("Terima kasih udah main sampai akhir. Love You Sayaang💕")
 
             else:
                 st.warning("Hehe coba pikir lagi... makanan kan favorit kamu 🥺")
