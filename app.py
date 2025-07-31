dc8f0a2d37af2d0b0b1ab5cba9b64cb6903ea8e4
nashilaaa <nashillanh@gmail.com>
2025-07-31 15:08:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -153,7 +153,7 @@ elif st.session_state.level == 6:
 
     if st.session_state.sudah_liat_lagu:
         st.header("💌 A Love Letter")
-        slow_print("""
+        st.markdown("""
 > Hai My Dearest,
 
 Selamat Ulang Tahun yaa sayang.. 
